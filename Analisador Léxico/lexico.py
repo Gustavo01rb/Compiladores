7c81f7778c1cf32e3111a9ae3cb979f52ddcd1bb
Gustavo Barcelos <gustavo05rb@gmail.com>
2022-05-23 10:51:57 -0300

    Correções analisador léxico

--- Analisador Léxico/lexico.py
+++ Analisador Léxico/lexico.py
@@ -15,33 +15,30 @@ class Lexico:
         except:
             raise ValueError("Falha ao abrir o arquivo {}, verifique se o mesmo existe!".format(self.__file_name))
 
-    def add_token(self,token, type):
+    def add_token(self,token, type, line):
         self.__tokens.append({
             "token" : token,
-            "type"  : type 
+            "type"  : type,
+            "line"  : line 
         })            
 
-    def __analyse_token(self, response, expression):
+    def __analyse_token(self, response, expression, line):
         operator           = response["match"][0]        # Pegando o operador a ser analisado 
         position_operator  = expression.find(operator)   # Pegando a posição do primeiro operador lógico da expressão 
 
         if expression == operator:
-            self.add_token(operator, response["type"])
+            self.add_token(operator, response["type"], line)
             return
 
 
         if position_operator > 0:
-            self.__validate_token(expression[:position_operator])             # Verificando o conteúdo que vem antes do operador
+            self.__validate_token(expression[:position_operator], line)             # Verificando o conteúdo que vem antes do operador
             
-        self.add_token(operator, response["type"])                            # Adiciona o operador a lista de tokens
-        self.__validate_token(expression[position_operator + len(operator):]) # Analisa partes depois do operador
+        self.add_token(operator, response["type"], line)                            # Adiciona o operador a lista de tokens
+        self.__validate_token(expression[position_operator + len(operator):], line) # Analisa partes depois do operador
 
 
-
-
-
-
-    def __validate_token(self, token,):
+    def __validate_token(self, token, line):
 
         if token == "" or token == None: return #Verificação de palavra vazia
         symbols = Symbols()
@@ -49,55 +46,60 @@ class Lexico:
         #Verificando se é palavra reservada 
         result = symbols.is_reserved_word(token)
         if result:
-            self.add_token(token, result["type"]) 
+            self.add_token(token, result["type"], line) 
             return
         
         #Verificando se há operadores lógicos
         result = symbols.is_logic_operator(token)
         if result:
-            self.__analyse_token(result, token)
+            self.__analyse_token(result, token, line)
             return
         
         #Verificando se há Delimitadores
         result = symbols.is_delimiter(token)
         if result:
-            self.__analyse_token(result, token)
+            self.__analyse_token(result, token, line)
             return
         
         #Verificando se há operador atitmético
         result = symbols.is_arithimetic_operator(token)
         if result:
-            self.__analyse_token(result, token)
+            self.__analyse_token(result, token, line)
             return      
         
         #Verificando se há Atribuição de igualdade
         result = symbols.is_assignment_operator(token)
         if result:
-            self.__analyse_token(result, token)
+            self.__analyse_token(result, token, line)
             return    
 
         if token.isnumeric():
-            self.add_token(token, "Constante Numérica")
+            self.add_token(token, symbols.numeric_constant, line)
             return
         
-        self.add_token(token, "Variável")
+        self.add_token(token, symbols.identifier, line)
         return
 
 
-
-
-
-        
-    
     #Função que gerencia a tokenização
     def tokenization(self):
-        for line in self.__content:
+        for index, line in enumerate(self.__content):
             line = line.strip() # Função que retira os espaços desnecesários ao final e início da string
             line = line.split() # Função que separa as stirngs. Nesse caso com base em " "
             for iterator in line:
                 iterator = iterator.strip()
-                self.__validate_token(iterator)
+                self.__validate_token(iterator, index)
+        self.__final_chek()
 
+    def __final_chek(self):
+        symbols = Symbols()
+        for index, iterator in enumerate(self.__tokens):
+            if symbols.is_library(iterator["token"]):
+                iterator['type'] = symbols.library
+                if(self.__tokens[index-1]['type'] == symbols.logic_operator):
+                    self.__tokens[index-1]['type'] = symbols.delimiter
+                if(self.__tokens[index+1]['type'] == symbols.logic_operator):
+                    self.__tokens[index+1]['type'] = symbols.delimiter
 
 
     def print_tokens(self):
--- Analisador Léxico/symbols.py
+++ Analisador Léxico/symbols.py
@@ -17,60 +17,76 @@ class Symbols:
     @property
     def delimiter(self):
         return "Delimitador"
+    @property
+    def library(self):
+        return "Biblioteca"
+    @property
+    def identifier(self):
+        return "Identificador"
+    @property
+    def numeric_constant(self):
+        return "Constante Numérica"
 
     #Funções de verficação
-    def get_reserved_word_list(self):
+    def __get_reserved_word_list(self):
         return ["const", "while", "for", "if", "#include", "stdio.h", "stdlib.h", "do", "return", "int", "string", "float", "double", "bool"]
-    def get_arithimetic_operator(self):
+    def __get_arithimetic_operator(self):
         return r'\+|-|\*|/|%'
-    def get_logic_operator(self):
+    def __get_logic_operator(self):
         return r'==|>=|<=|!=|!|<|>'
-    def get_assignment_operator(self):
+    def __get_assignment_operator(self):
         return r'='
-    def get_delimiter(self):
+    def __get_delimiter(self):
         return r'\(|\)|{|}|\[|]|;'
+    def __get_library(self):
+        return r'\.h$'
+
 
     
 
     #Funções de resposta
     def is_reserved_word(self, key):
-        if key in self.get_reserved_word_list():
+        if key in self.__get_reserved_word_list():
             return {
                 "type": self.reserved_word
                 }
         return False
     
     def is_arithimetic_operator(self, key):
-        if re.findall(self.get_arithimetic_operator(), key):
+        if re.findall(self.__get_arithimetic_operator(), key):
             return {
                 "type" : self.arithimetic_operator,
-                "match": re.findall(self.get_arithimetic_operator(), key)
+                "match": re.findall(self.__get_arithimetic_operator(), key)
                 }
         return False
     
     def is_logic_operator(self, key):
-        if re.findall(self.get_logic_operator(), key):
+        if re.findall(self.__get_logic_operator(), key):
             return {
                 "type"  : self.logic_operator,
-                "match" : re.findall(self.get_logic_operator(), key)
+                "match" : re.findall(self.__get_logic_operator(), key)
             }
         return False
         
     def is_assignment_operator(self, key):
-        if re.findall(self.get_assignment_operator(), key):
+        if re.findall(self.__get_assignment_operator(), key):
             return {
                 "type" : self.assignment_operator,
-                "match": re.findall(self.get_assignment_operator(), key)
+                "match": re.findall(self.__get_assignment_operator(), key)
             }
         return False
 
     def is_delimiter(self, key):
-        if re.findall(self.get_delimiter(), key):
+        if re.findall(self.__get_delimiter(), key):
             return {
                 "type" : self.delimiter,
-                "match": re.findall(self.get_delimiter(), key)
+                "match": re.findall(self.__get_delimiter(), key)
             }
         return False
+    
+    def is_library(self, key):
+        return re.search(self.__get_library(), key)
+        
 
 
 
